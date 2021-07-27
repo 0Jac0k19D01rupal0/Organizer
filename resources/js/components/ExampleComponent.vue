10c56ac6bf9620f6ff457a43673bc76bf2a1711c
koshceyshka <koshceyshka@gmail.com>
2021-07-27 11:09:36 +0300

    Include fullcalendar|Added Laravel Breeze with auth|Tested fullcalendar

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,40 @@
-<template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+import { Calendar } from '@fullcalendar/core';
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+// let calendarEl = document.getElementById('calendar');
+//
+// let calendar = new Calendar(calendarEl, {
+//     plugins: [ interactionPlugin ],
+//     // ...
+//         dateClick: function(info) {
+//     alert('Clicked on: ' + info.dateStr);
+//     alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+//     alert('Current view: ' + info.view.type);
+//     // change the day's background color just for fun
+//     info.dayEl.style.backgroundColor = 'red';
+// }
+// });
+
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth'
+            }
         }
     }
+}
+console.log('Works in vue')
+
 </script>
+<template>
+    <FullCalendar :options="calendarOptions" />
+</template>
